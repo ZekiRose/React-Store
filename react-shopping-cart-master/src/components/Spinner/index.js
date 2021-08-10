9d6094404e60f4afef90fbbdc2ce0a5450dd75f1
Manuel Iglesias <fncmacaroon@gmai.com>
2021-08-09 22:06:26 -0400

    Initial Check-In

---
+++ react-shopping-cart-master/src/components/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const fn = () =>(
+  <div className="spinner lds-ring">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+export default fn;
